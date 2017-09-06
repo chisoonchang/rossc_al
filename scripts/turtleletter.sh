84628feead7434c91d497cb3c83162a4e858dc0b
chisoonchang <chichangpcv@gmail.com>
2017-09-06 12:49:50 -0700

    Change my turtle msg

--- scripts/turtleletter.sh
+++ scripts/turtleletter.sh
@@ -1,2 +1,2 @@
 rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 0.0]' '[4.0, 5.0, 0.0]'
-rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[-5.0, 2.0, 0.0]' '[0.0, 0.0, 3.5]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[-4.0, 3.0, 0.0]' '[0.0, 0.0, 3.5]'
